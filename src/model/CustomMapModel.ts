946da1a0ca3256822b251a14a37d9f9e549ba82e
Tommy Zarick <zarick.6@osu.edu>
2020-10-14 22:50:16 -0400

    track preview added to info box - artist's top songs

--- src/model/CustomMapModel.ts
+++ src/model/CustomMapModel.ts
@@ -129,7 +129,9 @@ export class CustomMapModel {
         </ul>
         <iframe class="preview" id="${
           concert.artist.streamingId
-        }" src="" width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+        }" src="https://open.spotify.com/embed/artist/${
+        concert.artist.streamingId
+      }" width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
         </div>
         `;
 
@@ -149,12 +151,10 @@ export class CustomMapModel {
         if (!content.toString().includes('spotify:track')) {
           // add song preview iframe
           // open window -> load uri preview from spotify -> put it into the window once it arrives. So we don't freeze everything up while waiting
-          let content = infoWindow.getContent();
-          const newContent = this.getPreviewUris(content.toString()).then(
-            (response) => {
-              infoWindow.setContent(response);
-            }
-          );
+          // let content = infoWindow.getContent();
+          // this.getPreviewUris(content.toString()).then((response) => {
+          //   infoWindow.setContent(response);
+          // });
         }
         infoWindow.open(this.googleMap, marker);
         infoWindow.set('openStatus', true);
@@ -172,30 +172,32 @@ export class CustomMapModel {
     this.markers = [];
   }
 
-  private async getPreviewUris(content: string): Promise<string> {
-    const html = document.createElement('div');
-    html.innerHTML = content;
-    const iframes = html.getElementsByClassName('preview');
-    console.log(iframes.item(0)?.getAttribute('id'));
-    let i = 0;
-    let item = iframes.item(i);
-    while (item && i < 15) {
-      const uri = await this.getUri(item.getAttribute('id'));
-      if (uri) item.setAttribute('src', uri);
+  // private async getPreviewUris(content: string): Promise<HTMLDivElement> {
+  //   const html = document.createElement('div');
+  //   html.innerHTML = content;
+  //   const iframes = html.getElementsByClassName('preview');
+  //   // console.log(iframes.item(0)?.getAttribute('id'));
+  //   let i = 0;
+  //   let item = iframes.item(i);
+  //   while (item && i < 15) {
+  //     // only add previews for the first 15 shows that show up
+  //     const uri = await this.getUri(item.getAttribute('id'));
+  //     if (uri) item.setAttribute('src', uri);
+  //     item.setAttribute('src', 'spotify:track:4xvpttkjjEH0l2hrizYla7');
+  //     console.log(item);
+  //     i++;
+  //     item = iframes.item(i);
+  //   }
+  //   return html;
+  // }
 
-      i++;
-      item = iframes.item(i);
-    }
-    return '';
-  }
-
-  private async getUri(artistId: string | null): Promise<string | null> {
-    let uri: string | null = '';
-    if (artistId) {
-      // uri = await this.libraryReader.getPreviewUri(artistId);
-    }
-    return uri || '';
-  }
+  // private async getUri(artistId: string | null): Promise<string | null> {
+  //   let uri: string | null = '';
+  //   if (artistId) {
+  //     // uri = await this.libraryReader.getPreviewUri(artistId);
+  //   }
+  //   return uri || '';
+  // }
 
   private handleNewCoords(latLng: google.maps.LatLng): void {
     console.log(`lat: ${latLng.lat()}`);
